bd7688d6a930a425ed183e53a0f1c73b5fa56104
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-01-24 20:13:42 -0500

    Completed /todos and /completed routs

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import zod from 'zod';
-
+import { createTodo, updateTodo } from './types';
+import { Todo } from './db';
 
 const app = express();
 const PORT = 3000;
@@ -8,17 +8,50 @@ app.use(express.json());
 
 
 
-app.post("/todo", (req, res) => {
-    const title = req.body.title;
-    const discription = req.body.discription;
-    const realTitle = validTitle.safeParse(title);
-    const realDiscription = validDiscription.safeParse(discription);
+app.post("/todo", async (req, res) => {
+    const bodyPayload = req.body;
+    const validBody = createTodo.safeParse(bodyPayload);
+    if(!validBody.success) {
+        res.status(400).json({
+            messege: "You sent the wrong inputs"
+        });
+        return;
+    }
+    try{
+        await Todo.create({
+            title: bodyPayload.title,
+            description: bodyPayload.description,
+            completed: false
+        })
+        res.json({
+            messege: "todo created"
+        })
+    } catch{
+        res.status(500).json({
+            messege: "Some thing went wrong while creating the todo"
+        })
+    }
+    
 })
-app.get("/todos", (req, res) => {
-
+app.get("/todos", async (req, res) => {
+    const todos = await Todo.find();
+    res.json({
+        todos
+    })
 })
-app.put("/completed", (req, res) => {
-
+app.put("/completed", async (req, res) => {
+    const bodyPayload = req.body;
+    const validBody = updateTodo.safeParse(bodyPayload);
+    if(!validBody.success) {
+        res.status(400).json({
+            messege: "You sent the wrong inputs"
+        });
+        return;
+    }
+    await Todo.update({_id: bodyPayload.id}, {completed: true});
+    res.json({
+        messege: "Todo updated"
+    })
 })
 
 app.listen(PORT, () => {
