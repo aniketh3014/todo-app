71216f41788648366ae597f675db053005f4f43a
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-01-24 19:46:59 -0500

    Created database schema

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb+srv://admin:mypassword@cluster0.mcnh5ju.mongodb.net/todo-app');
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = {
+    Todo
+}
